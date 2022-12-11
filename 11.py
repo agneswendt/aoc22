8115888621e79fc8550c40be8f4ef346d3d8f983
Agnes Wendt <agnwe839@student.liu.se>
2022-12-11 19:06:51 +0100

    Day 11 rewrite

--- 11.py
+++ 11.py
@@ -7,34 +7,46 @@ def get_input():
     return [c[:-1] for c in lines]
 
 
+class monkey:
+    def __init__(self, items: list, op: str, test: str, m1: int, m2: int) -> None:
+        self.items = items
+        self.op = op
+        self.test = test
+        self.m1 = m1
+        self.m2 = m2
+        self.score = 0
+
+
 def solve(data):
     monkeys_raw = [
         list(g) for k, g in groupby(data, lambda x: x == "") if not k
     ]
     score = [0 for _ in range(len(monkeys_raw))]
-    monkeys = {}
-    for i, m in enumerate(monkeys_raw):
-        monkeys[i] = {
-            "items": list(map(int, m[1].split(": ")[-1].split(", "))),
-            "operation": m[2].split(": ")[-1].split("=")[-1],
-            "test": int(m[3].split(" ")[-1]),
-            "m1": int(m[4].split(" ")[-1]),
-            "m2": int(m[5].split(" ")[-1]),
-        }
+
+    monkeys = []
+    for m in monkeys_raw:
+        monkeys.append(
+            monkey(
+                items=list(map(int, m[1].split(": ")[-1].split(", "))),
+                op=m[2].split(": ")[-1].split("=")[-1],
+                test=int(m[3].split(" ")[-1]),
+                m1=int(m[4].split(" ")[-1]),
+                m2=int(m[5].split(" ")[-1]),
+            )
+        )
 
     for _ in range(10000):
         for m in monkeys:
-            for item in monkeys[m]["items"]:
-                score[m] += 1
+            for item in m.items:
+                m.score += 1
                 old = item
-                # new = eval(monkeys[m]["operation"]) // 3
-                new = eval(monkeys[m]["operation"]) % 9699690  # 🤡
-                if new % monkeys[m]["test"]:
-                    monkeys[monkeys[m]["m2"]]["items"].append(new)
+                new = eval(m.op) % 9699690  # 🤡
+                if new % m.test:
+                    monkeys[m.m2].items.append(new)
                 else:
-                    monkeys[monkeys[m]["m1"]]["items"].append(new)
-            monkeys[m]["items"] = []
-    score = list(reversed(sorted(score)))
+                    monkeys[m.m1].items.append(new)
+            m.items = []
+    score = list(reversed(sorted(m.score for m in monkeys)))
     print(f"Part 2: {score[0] * score[1]}")
 
 
